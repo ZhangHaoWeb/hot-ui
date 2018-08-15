27a80f3e0b609b377e0c1036ebc429d05d7e8751
zhanghaov <48798724@qq.com>
2018-08-15 18:57:24 +0800

    add theme sass

---
+++ packages/theme-default/gulpfile.js
@@ -0,0 +1,21 @@
+'use strict'
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssmin = require('gulp-cssmin');
+
+gulp.task('compile', function() {
+    return gulp.src('./src/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./lib'))
+});
+
+gulp.task('copyfont', function() {
+    return gulp.src('./src/fonts/**')
+      .pipe(cssmin())
+      .pipe(gulp.dest('./lib/fonts'));
+});
+
+gulp.task('build', ['compile', 'copyfont']);
+gulp.task('watch', function () {
+  gulp.watch('./src/*.scss', ['compile']);
+});
