09a33640c950f2bf582a46cba0fc04e9b4db00be
zhanghao <48798724@qq.com>
2023-04-25 16:31:16 +0800

    update compile sass

--- packages/theme-default/gulpfile.js
+++ packages/theme-default/gulpfile.js
@@ -1,11 +1,14 @@
 'use strict'
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 gulp.task('compile', function() {
     return gulp.src('./src/*.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssmin())
         .pipe(gulp.dest('./lib'))
 });
 
